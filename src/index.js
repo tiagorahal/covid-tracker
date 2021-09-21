6b8de275e67e4e56e9430a2acf9112fffdb53b9f
tiagorahal <rahal.aires@gmail.com>
2021-09-21 13:53:01 -0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './feature/redux/slices/configStore';
+import store from './redux/configstore';
 
 ReactDOM.render(
   <React.StrictMode>
